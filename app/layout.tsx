0c184d29001e4522e10576ec6a4ca4f0d19cc91f
DimitarBelchev <heavyartillery98@gmail.com>
2025-01-14 23:28:51 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
-import Navbar from '@/components/navbar/Navbar';
-import Container from '@/components/global/Container';
-import Providers from './providers';
-import { ClerkProvider } from '@clerk/nextjs';
-const inter = Inter({ subsets: ['latin'] });
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import Container from "@/components/global/Container";
+import Providers from "./providers";
+import { ClerkProvider } from "@clerk/nextjs";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Next Storefront',
-  description: 'A nifty store built with Next.js',
+  title: "NextJS Storefront",
+  description: "A nifty store built with Next.js",
 };
 
 export default function RootLayout({
@@ -19,11 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang='en' suppressHydrationWarning>
+      <html lang="en" suppressHydrationWarning>
         <body className={inter.className}>
           <Providers>
             <Navbar />
-            <Container className='py-20'>{children}</Container>
+            <Container className="py-20">{children}</Container>
           </Providers>
         </body>
       </html>
