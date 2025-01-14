ccf4b618bd9a2d203883e88fc44ac432a649c8ca
DimitarBelchev <heavyartillery98@gmail.com>
2025-01-14 22:56:30 +0200

    update

--- app/admin/Sidebar.tsx
+++ app/admin/Sidebar.tsx
@@ -1,30 +1,28 @@
-'use client';
-import { adminLinks } from '@/utils/links';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { Button } from '@/components/ui/button';
-
-function Sidebar() {
+"use client";
+import { adminLinks } from "@/utils/links";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
+const Sidebar = () => {
   const pathname = usePathname();
 
   return (
     <aside>
       {adminLinks.map((link) => {
         const isActivePage = pathname === link.href;
-        const variant = isActivePage ? 'default' : 'ghost';
+        const variant = isActivePage ? "default" : "ghost";
         return (
           <Button
             asChild
-            className='w-full mb-2 capitalize font-normal justify-start'
+            className="w-full mb-2 capitalize font-normal justify-start"
             variant={variant}
+            key={link.href}
           >
-            <Link key={link.href} href={link.href}>
-              {link.label}
-            </Link>
+            <Link href={link.href}>{link.label}</Link>
           </Button>
         );
       })}
     </aside>
   );
-}
+};
 export default Sidebar;
--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,5 +1,5 @@
-import { LuUser2 } from 'react-icons/lu';
-import { currentUser } from '@clerk/nextjs/server';
+import { LuUser } from "react-icons/lu";
+import { currentUser } from "@clerk/nextjs/server";
 
 async function UserIcon() {
   const user = await currentUser();
@@ -8,10 +8,10 @@ async function UserIcon() {
 
   if (profileImage) {
     return (
-      <img src={profileImage} className='w-6 h-6 rounded-full object-cover' />
+      <img src={profileImage} className="w-6 h-6 rounded-full object-cover" />
     );
   }
 
-  return <LuUser2 className='w-6 h-6 bg-primary rounded-full text-white' />;
+  return <LuUser className="w-6 h-6 bg-primary rounded-full text-white" />;
 }
 export default UserIcon;
